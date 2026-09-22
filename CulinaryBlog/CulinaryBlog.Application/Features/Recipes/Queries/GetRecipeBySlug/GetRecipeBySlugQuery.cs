d7c03455aa10e9b8ffb1f3ddf2ac9f19a329abd4
AKhoaLe <anhkhoa080205@gmail.com>
2026-09-22 15:24:00 +0700

    feat(recipes): implement FR-RCP-002 - xem chi tiết công thức nấu ăn

---
+++ CulinaryBlog/CulinaryBlog.Application/Features/Recipes/Queries/GetRecipeBySlug/GetRecipeBySlugQuery.cs
@@ -0,0 +1,6 @@
+using CulinaryBlog.Application.DTOs;
+using MediatR;
+
+namespace CulinaryBlog.Application.Features.Recipes.Queries.GetRecipeBySlug;
+
+public record GetRecipeBySlugQuery(string Slug) : IRequest<RecipeDetailDto?>;
